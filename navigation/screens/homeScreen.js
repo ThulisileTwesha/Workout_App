e35bf4976fff26edab534c286cbc50ddcaf534fe
Thulisile Twesha <tweshathulisile@gmail.com>
2023-11-13 15:40:24 +0200

    first commit

---
+++ navigation/screens/homeScreen.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function homeScreen({navigation}){
+    return(
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <Text
+        onPress={() => alert('This is home screen')}
+            style={{fontsize:26, fontWeight: bold}}>HomeScreen</Text>
+        <Statusbar style='auto'/>
+        
+      </View>
+    );
+}
