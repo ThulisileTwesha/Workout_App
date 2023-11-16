d95eca94085bd6b013dc83bcaa3d36423293a3c3
Thulisile Twesha <tweshathulisile@gmail.com>
2023-11-16 10:54:00 +0200

    Thursday

--- navigation/screens/homeScreen.js
+++ navigation/screens/homeScreen.js
@@ -1,31 +1,42 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { StyleSheet } from 'react-native';
-import WeeklyCalendar from 'react-native-weekly-calendar';
+//import WeeklyCalendar from 'react-native-weekly-calendar';
+import { DatePicker } from 'react-native-week-month-date-picker';
+import { addDays } from 'date-fns';
 
 
 export default function HomeScreen(){
-    const sampleEvents = [
-        { 'start': '2023-11-13 ', 'note': 'Full Body' },
-        { 'start': '2023-11-14 ',  'note': 'Abs and Cardio' },
-        { 'start': '2023-11-15 ',  'note': 'Leg Day' },
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+   // const sampleEvents = [
+        //{ 'start': '2023-11-13 ', 'note': 'Full Body' },
+        //{ 'start': '2023-11-14 ',  'note': 'Abs and Cardio' },
+        //{ 'start': '2023-11-15 ',  'note': 'Leg Day' },
        
-      ];
+      //];
 
    
     return(
         <View StyleSheet = {StyleSheet.container}>
         <Text style={styles.setFontSizeOne}>Welcome Back Thuli</Text>
-        <Text style={styles.setFontSizeOne}>Weekly Goal</Text>
-        <WeeklyCalendar
-        events={sampleEvents}
-        selected="2023-11-13"
-        startWeekday={0}
-        weekdayFormat="ddd"
-        locale="en"
-        style={{ height: 400 }}
-      />
-       
+
+        <SafeAreaView>
+      <DatePicker
+        startDate={new Date()}
+        maxFutureDays={90}
+        markedDates={[new Date(), addDays(new Date(), 2)]}
+        onDateChange={(date) => setSelectedDate(date)}
+        theme={{
+          primaryColor: 'purple',
+        }}
+      >
+        <View>
+          <Text>Timeslots</Text>
+          <Text>{selectedDate.toString()}</Text>
+        </View>
+      </DatePicker>
+    </SafeAreaView>
+    
             
        
         
